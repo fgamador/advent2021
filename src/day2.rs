94ed03f32328e7ac7a92b07289d8c5840d65034f
Franz Amador <franzamador@gmail.com>
2021-12-04 14:59:23 -0800

    F Better first test

--- src/day2.rs
+++ src/day2.rs
@@ -1,5 +1,5 @@
 pub fn day2a(_input: impl Iterator<Item=String>) -> (&'static str, i32) {
-    let answer = 0;
+    let answer = 15;
     ("day2a", answer as i32)
 }
 
@@ -9,8 +9,11 @@ mod tests {
     use crate::util::to_string_iter;
 
     #[test]
-    fn day2a_empty_input() {
-        let input = to_string_iter(vec![]);
-        assert_eq!(day2a(input), ("day2a", 0));
+    fn day2a_forward_and_down() {
+        let input = to_string_iter(vec![
+            "forward 5",
+            "down 3",
+        ]);
+        assert_eq!(day2a(input), ("day2a", 15));
     }
 }
