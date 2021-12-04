3661f859c195f3dd5a63bf70d780afe16594aaa6
Franz Amador <franzamador@gmail.com>
2021-12-04 14:42:32 -0800

    F day1b done

--- src/day1.rs
+++ src/day1.rs
@@ -9,9 +9,20 @@ pub fn day1a(input: impl Iterator<Item=String>) -> (&'static str, i32) {
     ("day1a", answer as i32)
 }
 
+pub fn day1b(input: impl Iterator<Item=String>) -> (&'static str, i32) {
+    let answer = input
+        .map(|line| line.parse::<u32>().unwrap())
+        .tuple_windows::<(_, _, _)>()
+        .map(|(d1, d2, d3)| d1 + d2 + d3)
+        .tuple_windows()
+        .filter(|(d1, d2)| d2 > d1)
+        .count();
+    ("day1b", answer as i32)
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::day1::day1a;
+    use crate::day1::*;
     use crate::util::to_string_iter;
 
     #[test]
@@ -35,5 +46,22 @@ mod tests {
             "263",
         ]);
         assert_eq!(day1a(input), ("day1a", 7));
+    }
+
+    #[test]
+    fn day1b_example_input() {
+        let input = to_string_iter(vec![
+            "199",
+            "200",
+            "208",
+            "210",
+            "200",
+            "207",
+            "240",
+            "269",
+            "260",
+            "263",
+        ]);
+        assert_eq!(day1b(input), ("day1b", 5));
     }
 }
