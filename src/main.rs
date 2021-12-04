c63ecd4dbec5e95b9acfad5d4c1f4978c0a86850
Franz Amador <franzamador@gmail.com>
2021-12-04 14:29:41 -0800

    F** Switch main to call day1a

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,11 @@ mod util;
 mod day1;
 
 fn main() {
-    let (day, answer) = day0(util::read_input_file());
+    let (day, answer) = day1::day1a(util::read_input_file());
     println!("{} answer: {}", day, answer);
 }
 
-fn day0<I>(_input: I) -> (&'static str, i32)
+fn _day0<I>(_input: I) -> (&'static str, i32)
     where I: Iterator<Item=String>
 {
     ("day0", 0)
