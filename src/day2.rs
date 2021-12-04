9c98411e6717fef1952d0bb00572eeb8e2e23262
Franz Amador <franzamador@gmail.com>
2021-12-04 15:42:30 -0800

    R!! Extract struct

--- src/day2.rs
+++ src/day2.rs
@@ -1,6 +1,21 @@
 pub fn day2a(_input: impl Iterator<Item=String>) -> (&'static str, i32) {
-    let answer = 15;
+    let state = SubState::new(3, 5);
+    let answer = state.hpos * state.depth;
     ("day2a", answer as i32)
+}
+
+struct SubState {
+    pub hpos: i32,
+    pub depth: i32,
+}
+
+impl SubState {
+    pub fn new(hpos: i32, depth: i32) -> Self {
+        SubState {
+            hpos,
+            depth,
+        }
+    }
 }
 
 #[cfg(test)]
