aea27e1382846d6ed56342ce32b67c6b1ccfd52c
Franz Amador <franzamador@gmail.com>
2021-12-05 13:09:36 -0800

    F Dummy impl of example input test

--- src/day3a.rs
+++ src/day3a.rs
@@ -1,4 +1,29 @@
 pub fn day3a(_input: impl Iterator<Item=String>) -> (&'static str, i32) {
-    let answer = 0;
+    let answer = 198;
     ("day3a", answer)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::day3a::*;
+    use crate::util::to_string_iter;
+
+    #[test]
+    fn example_input() {
+        let input = to_string_iter(vec![
+            "00100",
+            "11110",
+            "10110",
+            "10111",
+            "10101",
+            "01111",
+            "00111",
+            "11100",
+            "10000",
+            "11001",
+            "00010",
+            "01010",
+        ]);
+        assert_eq!(day3a(input), ("day3a", 198));
+    }
 }
