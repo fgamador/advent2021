d0dea08e16c6f8f0fcd4bc059206197064085ef9
Franz Amador <franzamador@gmail.com>
2021-12-05 11:25:36 -0800

    r extract vars

--- src/day2b.rs
+++ src/day2b.rs
@@ -1,7 +1,9 @@
 use itertools::Itertools;
 
 pub fn day2b(_input: impl Iterator<Item=String>) -> (&'static str, i32) {
-    let answer = 3 * 6;
+    let state_hpos = 3;
+    let state_depth = 6;
+    let answer = state_hpos * state_depth;
     ("day2b", answer)
 }
 
