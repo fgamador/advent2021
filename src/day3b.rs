8c370a59662bb16804a6a62a09b2dad80fbd096a
Franz Amador <franzamador@gmail.com>
2021-12-18 18:42:12 -0800

    r Rename param

--- src/day3b.rs
+++ src/day3b.rs
@@ -34,8 +34,8 @@ fn find_og_rating_bits(input_bitvecs: &[Vec<bool>]) -> Vec<bool> {
     chosen_bitvecs[0].clone()
 }
 
-fn winnow_bitvecs(input_bitvecs: &[Vec<bool>], bit_index: usize) -> Vec<Vec<bool>> {
-    let (true_bitvecs, false_bitvecs) = partition_bitvecs_by_bit_value(input_bitvecs, bit_index);
+fn winnow_bitvecs(bitvecs: &[Vec<bool>], bit_index: usize) -> Vec<Vec<bool>> {
+    let (true_bitvecs, false_bitvecs) = partition_bitvecs_by_bit_value(bitvecs, bit_index);
 
     if true_bitvecs.len() == false_bitvecs.len() {
         true_bitvecs
