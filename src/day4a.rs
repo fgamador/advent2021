26301322d185245358b4565ee7471fad3d4f8277
Franz Amador <franzamador@gmail.com>
2021-12-19 16:52:48 -0800

    R** Remove redundant syntax

--- src/day4a.rs
+++ src/day4a.rs
@@ -4,7 +4,7 @@ use itertools::Itertools;
 pub fn day4a(input: impl Iterator<Item=String>) -> (&'static str, i32) {
     let mut boards = load_boards(input.skip(1));
     let (winning_board, winning_number) = play_boards(&mut boards);
-    let winning_score: u32 = winning_board.sum_unmarked_numbers();
+    let winning_score = winning_board.sum_unmarked_numbers();
     let answer = winning_score * winning_number;
     ("day4a", answer as i32)
 }
