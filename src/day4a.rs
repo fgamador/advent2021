70cfb70665264a22f4d4c1ebb1f4e6be5f9d6d12
Franz Amador <franzamador@gmail.com>
2021-12-25 13:15:59 -0800

    T (failing) test for day 4b

--- src/day4a.rs
+++ src/day4a.rs
@@ -14,13 +14,14 @@ pub fn day4a(mut input: impl Iterator<Item=String>) -> (&'static str, i32) {
     }
 }
 
-#[allow(dead_code)]
 pub fn day4b(mut input: impl Iterator<Item=String>) -> (&'static str, i32) {
     let numbers = parse_numbers_csv(&input.next().unwrap());
     let mut boards = read_boards(input);
-    if let Some((winning_board_index, _winning_number)) = play_to_last_winning_board(&mut boards, &numbers) {
+    if let Some((winning_board_index, winning_number)) = play_to_last_winning_board(&mut boards, &numbers) {
         let winning_score = boards[winning_board_index].sum_unmarked_numbers();
-        ("day4b", winning_score as i32)
+        println!("Number: {}, score: {}", winning_number, winning_score);
+        let answer = winning_score * winning_number;
+        ("day4b", answer as i32)
     } else {
         panic!("No winning board");
     }
@@ -277,7 +278,7 @@ mod tests {
     }
 
     #[test]
-    fn example_input() {
+    fn example_input_4a() {
         let input = to_string_iter(vec![
             "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1",
             "",
@@ -300,5 +301,32 @@ mod tests {
             " 2  0 12  3  7",
         ]);
         assert_eq!(day4a(input), ("day4a", 4512));
+    }
+
+    #[test]
+    #[ignore]
+    fn example_input_4b() {
+        let input = to_string_iter(vec![
+            "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1",
+            "",
+            "22 13 17 11  0",
+            " 8  2 23  4 24",
+            "21  9 14 16  7",
+            " 6 10  3 18  5",
+            " 1 12 20 15 19",
+            "",
+            " 3 15  0  2 22",
+            " 9 18 13 17  5",
+            "19  8  7 25 23",
+            "20 11 10 24  4",
+            "14 21 16 12  6",
+            "",
+            "14 21 17 24  4",
+            "10 16 15  9 19",
+            "18  8 23 26 20",
+            "22 11 13  6  5",
+            " 2  0 12  3  7",
+        ]);
+        assert_eq!(day4b(input), ("day4b", 1924));
     }
 }
